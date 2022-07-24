5ea3568e2b6b343de6b1d3945296b91fe62d9821
Rianto <rianto.rnt@gmail.com>
2022-07-24 16:40:29 +0800

    composing component, passing data to components, passing children

--- counter/src/index.js
+++ counter/src/index.js
@@ -5,12 +5,13 @@ import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import Counter from "./components/counter";
+// import Counter from "./components/counter";
+import Counters from "./components/counters";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>
 );
 
