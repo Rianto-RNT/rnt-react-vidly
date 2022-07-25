e2c698eb315199ef273399d44b0528048be9e4c2
Rianto <rianto.rnt@gmail.com>
2022-07-25 12:48:52 +0800

    Debugging our apps, props-state, raising and handling events, updating the state, single source of thruth, removing the local state

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -1,41 +1,45 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.value + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span style={{ color: "#222222" }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={(product) => this.handleIncrement(product)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-primary btn-sm"
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.props.counter.value === 0 ? "warning" : "success";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
