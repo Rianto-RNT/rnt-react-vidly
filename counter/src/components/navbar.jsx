7467516120d06c1a94425a9ed5bd595ecaff0c10
Rianto <rianto.rnt@gmail.com>
2022-07-26 15:36:27 +0800

    Multiple component in sync, Lifting the state up, stateless fuctional component, destructuring arguments, lifecycle hooks

---
+++ counter/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// Stateless Functional component
+
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1" href="#">
+          Navbar
+          <span className="badge badge-pill bg-secondary m-3">
+            {totalCounters}
+          </span>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
