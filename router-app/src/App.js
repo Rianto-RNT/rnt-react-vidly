333323ef83edd6faa48cff178483803b6c1fc4e9
Rianto <rianto.rnt@gmail.com>
2022-08-01 14:09:49 +0800

    implementing Switch, Link, route props, route paramenter, optional parameter, query string

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/navbar";
 import Products from "./components/products";
@@ -16,10 +16,16 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            />
+            <Route path="/posts/:year?/:month?" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
